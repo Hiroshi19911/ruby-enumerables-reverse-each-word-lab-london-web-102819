e69d6d6e64a07dda4a26928ba385ba4e588cfd05
Hiroshi Sipahioglu <hiroshis91@icloud.com>
2019-10-22 23:27:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,11 @@
-def reverse_each_word(sentence1)
-  reverse = "Hello there, and how are you?"
-  puts reverse.reverse
-end 
+def reverse_each_word(string)
+    new_arr = []
+    arr = string.split(" ")
+    arr.each { |i| new_arr << i.reverse }
+    new_arr.join(" ")
+end
 
+# Reverse each word using #collect
+def reverse_each_word_collect(string)
+    string.split(" ").collect { |i| i.reverse }.join(" ")
+end
